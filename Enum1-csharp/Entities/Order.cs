efc25b323959df3fe4f61413bad6928588ae25dd
JnickU <nicknickeado1@gmail.com>
2021-07-26 15:49:45 -0300

    Primeira Implementação (Class Order, Enum OrderStatus)

---
+++ Enum1-csharp/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Enum1_csharp.Entities.Enums;
+
+namespace Enum1_csharp.Entities {
+    class Order {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return Id
+                   + ", "
+                   + Moment
+                   + ", "
+                   + Status;
+        }
+
+    }
+}
--- Enum1-csharp/Program.cs
+++ Enum1-csharp/Program.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using Enum1_csharp.Entities;
+using Enum1_csharp.Entities.Enums;
 
 namespace Enum1_csharp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
         }
     }
 }
